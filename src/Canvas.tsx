18ad91990dffd806fb2a8a3081dff7c55d21bf6e
akifozturk61 <38880860+akifozturk61@users.noreply.github.com>
2024-08-05 18:58:00 +0200

    bug with overlapping shapes

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -35,16 +35,18 @@ const Canvas = (props: CanvasProps) => {
     for (const shape of shapesRef.current) {
       if (shape.isPointInsideShape(new Point(x, y))) {
         //TODO remove hardcarded shape in first position of array find another way to identify it
-
         if (shape == shapesRef.current[0]) {
           props.incrementscore(scoreIncrementChosen);
           scoreRef.current = props.score;
+          break;
         } else if (shape == shapesRef.current[shapesRef.current.length - 1]) {
           props.incrementscore(scoreIncrementObject);
           scoreRef.current = props.score;
+          break;
         } else {
           props.incrementscore(scoreIncrementVariation);
           scoreRef.current = props.score;
+          break;
         }
       }
     }
